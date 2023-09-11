c3812e7624c900b832328e394540778eca81914c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-09-11 19:42:42 +0530

    StackArrayImplemention

--- src/Stack/StackArrayImplemention.java
+++ src/Stack/StackArrayImplemention.java
@@ -1,4 +1,15 @@
 package Stack;
 
 public class StackArrayImplemention {
+    int top;
+    int capacity;
+    int[] stack;
+    StackArrayImplemention(){
+        top=-1;
+        capacity=10;
+        stack=new int[capacity];
+    }
+    public static void main(String[] args) {
+
+    }
 }
